0a5410ccb1f4e6e7a21d8d3fad6e424188015030
Gurdeepsingh-10 <getmegurdeep@gmail.com>
2026-01-13 12:14:57 +0530

    added main and chat_route file

--- src/handlers/chat_handler.py
+++ src/handlers/chat_handler.py
@@ -1,2 +1,11 @@
-def hello():
-    return "hello"
+from src.agents.chat_agent.graph import create_chat_agent_graph
+graph = create_chat_agent_graph()
+def chat_agent_handler(message: str) -> dict[str,str]:
+    """
+    Docstring for chat_agent_handler
+    
+    :return: Description
+    :rtype: dict[str, str]
+
+    """
+    return graph.invoke({"messages": message}) #{messages: "response from chat agent"}
--- src/routes/route.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from src.handlers.handler import hello
-router = APIRouter()
-
-
-@router.get("/")
-def hello_route():
-    return hello()
