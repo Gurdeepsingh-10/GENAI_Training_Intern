55dce1af2a0c834bdc1f265c5155de9bf5293c6c
Gurdeepsingh-10 <getmegurdeep@gmail.com>
2026-01-13 11:10:56 +0530

    changed: handler -> chat_handler

--- src/agents/chat_agent/graph.py
+++ src/agents/chat_agent/graph.py
@@ -6,7 +6,12 @@ from src.agents.chat_agent.nodes.chat_node import chat
 
 
 def create_chat_agent_graph() -> CompiledStateGraph:
+    """
+    Docstring for create_chat_agent_graph
     
+    :return: Description
+    :rtype: CompiledStateGraph[Any, None, Any, Any]
+    """
     graph_builder = StateGraph(ChatAgentState)
     graph_builder.add_node("chat_node", chat)
     graph_builder.add_edge(START,"chat_node")
