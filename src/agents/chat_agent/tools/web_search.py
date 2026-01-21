6acff23f60398444caad2283200481b3e4e4497c
Gurdeepsingh-10 <getmegurdeep@gmail.com>
2026-01-21 15:20:41 +0530

    fix: web search files fixed

--- src/agents/chat_agent/tools/web_search.py
+++ src/agents/chat_agent/tools/web_search.py
@@ -4,15 +4,16 @@ from langchain.tools import tool
 search = DuckDuckGoSearchRun()
 
 @tool
-def search_the_web (query : str) ->str:
+def search_the_web(query: str) -> str:
     """
-    Docstring for search_the_web
-    
-    use this tool to search the web
-    :param query: Description
-    :type query: str
-    :return: Description
-    :rtype: str
+    Search the web for current and up-to-date information based on a query.
     """
+    results = search(query, max_results=3)
 
-    return search.invoke(query)
+    summaries = []
+    for r in results:
+        summaries.append(
+            f"{r['title']}: {r['body'][:250]}"
+        )
+
+    return "\n".join(summaries)
